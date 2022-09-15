b4fb6a72c95d01049c8c12d9a370489198455d6a
jor-eldevera <jorell.devera@gmail.com>
2022-09-14 17:44:14 -0700

    Add setter and getter to gameBoard

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 const gameBoard = (function() {
+    // 6 | 7 | 8
+    // ---------
+    // 3 | 4 | 5
+    // ---------
+    // 0 | 1 | 2
     let gameBoard = ["X", "O", "X", "X", "O", "O", "X", "X", "O"];
 
-    return {
+    function setBoardSpot(spot, letter) {
+        if (spot < 0 || spot > 8) {
+            console.error("setBoardSpot: Spot must be a number between 0-8 inclusive");
+        }
 
+        if (letter !== "X" || letter !== "O") {
+            console.error("setBoardSpot: Letter must be X or O");
+        }
+
+        gameBoard[spot] = letter;
+    }
+
+    function getBoardSpot(spot) {
+        if (spot < 0 || spot > 8) {
+            console.error("getBoardSpot: Spot must be a number between 0-8 inclusive");
+        }
+
+        return gameBoard[spot];
+    }
+
+    return {
+        setBoardSpot: setBoardSpot,
+        getBoardSpot: getBoardSpot
     };
 })();
 
-const gameController = (function() {
+const displayController = (function() {
 
     return {
 
