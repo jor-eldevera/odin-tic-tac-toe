b5d76a0d9a45eb99c99983b373ec80f07b07c33b
jor-eldevera <jorell.devera@gmail.com>
2022-10-04 14:58:09 -0700

    Add setDOMSpot in display controller

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const gameBoard = (function() {
-    // 6 | 7 | 8
+    // 0 | 1 | 2
     // ---------
     // 3 | 4 | 5
     // ---------
-    // 0 | 1 | 2
+    // 6 | 7 | 8
     let gameBoard = ["X", "O", "X", "X", "O", "O", "X", "X", "O"];
 
     function setBoardSpot(spot, letter) {
@@ -35,7 +35,36 @@ const gameBoard = (function() {
 const displayController = (function() {
     const gameBoardDOM = document.getElementById("gameBoard");
     let spots = gameBoardDOM.children;
+
+    function setDOMSpot(spot, mark) {
+        if (spot > 8) {
+            console.error("setDOMSpot: Spot must be a number between 0-8 inclusive. Spot is " + spot);
+            return;
+        }
+
+        if (mark !== "X" && mark !== "O") {
+            console.error("setDOMSpot: Mark must be X or O. Mark is " + mark);
+            return;
+        }
+
+        for (let i = 0; i <= 8; i++) {
+            if (i === spot) {
+                spots[i].textContent = mark; 
+            }
+        }
+    }
+
     return {
-        
+        setDOMSpot: setDOMSpot
     };
 })();
+
+const gameController = (function() {
+
+    return {
+
+    }
+})();
+
+displayController.setDOMSpot(1, "X");
+displayController.setDOMSpot(8, "O");
